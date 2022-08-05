1fd983c0ebb7712f028f8eab804b5f2fb78312b0
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-08-06 02:38:42 +0600

    save

--- src/data/fake_sub_category.js
+++ src/data/fake_sub_category.js
@@ -30,41 +30,177 @@ export const fake_sub_category = {
                 title:'гипсокартон профиль гвл',
                 image:foto,
                 id:2,
+                materials:[
+                    {
+                        title:'Banka',
+                        img:Banka,
+                        price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                ]
             },
             {
                 title:'грунтовка и бетоноконтакт',
                 image:foto,
                 id:3,
+                materials:[
+                    {
+                        title:'Banka',
+                        img:Banka,
+                        price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                ]
             },
             {
                 title:'клей для блоков',
                 image:foto,
                 id:4,
+                materials:[
+                    {
+                        title:'Banka',
+                        img:Banka,
+                        price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                ]
             },
             {
                 title:'пеноблоки,пгп,кирпич',
                 image:foto,
                 id:5,
+                materials:[
+                    {
+                        title:'Banka',
+                        img:Banka,
+                        price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                ]
             },
             {
                 title:'плиточный клей и затирка',
                 image:foto,
                 id:6,
+                materials:[
+                    {
+                        title:'Banka',
+                        img:Banka,
+                        price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                ]
             },
             {
                 title:'сетки маляр,серпянки,скотч',
                 image:foto,
                 id:7,
+                materials:[
+                    {
+                        title:'Banka',
+                        img:Banka,
+                        price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                ]
             },
             {
                 title:'шпатлевка',
                 image:foto,
                 id:8,
+                materials:[
+                    {
+                        title:'Banka',
+                        img:Banka,
+                        price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                ]
             },
             {
                 title:'штукатурка',
                 image:foto,
                 id:9,
+                materials:[
+                    {
+                        title:'Banka',
+                        img:Banka,
+                        price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                    {
+                     title:'Banka',
+                     img:Banka,
+                     price:200
+                    },
+                ]
             }
         ],
       
